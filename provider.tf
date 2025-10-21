f07d6ae8c5635c4cb1ddf8c7f7f7cba2e2041457
ammarin <ammarinpobsuk@gmail.com>
2025-10-21 15:55:14 +0700

    fix: correct typo in provider source for azurerm

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform{
     required_providers{
         azurerm = {
-            source = "hasicorp/azurerm"
+            source = "hashicorp/azurerm"
             verison = "~> 3.0"
         }
     }
